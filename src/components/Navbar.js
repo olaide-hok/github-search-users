a24b4d71ecee2a5c33d2ec8f66af71b460c4538d
olaide-hok <olaidekareem43@yahoo.com>
2022-06-14 16:53:21 +0100

    Setup GET requests for Repos and Followers

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
   return <Wrapper>navbar component</Wrapper>;
