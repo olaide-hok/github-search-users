05e590ff98c56cc450c4255420522427d29a6acb
olaide-hok <olaidekareem43@yahoo.com>
2022-06-17 14:41:20 +0100

    Change keycloak realm

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -3,17 +3,11 @@ import { Navigate } from 'react-router-dom'
 import AuthKeycloakService from '../AuthService/AuthKeycloak'
 import Login from './Login'
 
-// const {  isLoggedIn, kc } = AuthKeycloakService
-// const { authenticated } = kc
-// const user = isLoggedIn() && authenticated
-// console.log(authenticated)
-// console.log(user);
-
 
 const PrivateRoute = ({children}) => {
   const {  isLoggedIn, kc } = AuthKeycloakService
 const { authenticated } = kc
-  return authenticated ? children : <Login/>
+  return isLoggedIn() ? children : <Login/>
   // return !isLoggedIn() ? <Login/> : children
   
 
